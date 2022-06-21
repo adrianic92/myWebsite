57c0a43e3576c19adf5f96e1e992fb9e1448fde6
Adrian <adrianic92@yahoo.com>
2022-06-20 22:54:38 -0700

    added color change for mouseover and mouseout

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Hello, world. This is my first website.";
 document.querySelector("body").appendChild(h2);
+
+document.getElementById("hulk").onmouseover = function() {mouseOver()};
+document.getElementById("hulk").onmouseout = function() {mouseOut()};
+function mouseOver() {
+    document.getElementById("hulk").style.color = "green";
+}
+function mouseOut() {
+    document.getElementById("hulk").style.color = "white";
+}
