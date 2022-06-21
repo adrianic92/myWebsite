322fc4eb947757c76534ab450898e9ad7482817e
Adrian <adrianic92@yahoo.com>
2022-06-20 22:24:32 -0700

    added image and some text

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Hello, world. This is my first website.";
 document.querySelector("body").appendChild(h2);
